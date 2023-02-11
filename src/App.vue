e817e8dfe5141c3294a39471a262dcc607aa4b0e
Silvia Maranzan <silvia@Air-di-Smaranza.local>
2023-02-11 14:09:25 +0100

    SM: FIRST commit, setting up environment

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld user="User"/>
+  <h3>Subtitle <i class="bi-play-circle"></i></h3>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import './scss/app.scss'; // Import app styling
 </style>
